6462e27f52ef7eebc707775d2edcbde33432e6f8
HuoYanru <408074739@qq.com>
2017-12-27 10:33:51 +0800

    hyr-js

---
+++ js/mall.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper ('.swiper-container', {
+	//分页器
+    pagination: '.swiper-pagination',
+//          paginationClickable: true,
+    speed: 1000,
+    loop: true,
+//          observer:true,
+//          observeParents:true,
+	//自动轮播
+    autoplayDisableOnInteraction : false,
+    autoplay:1500
+})
