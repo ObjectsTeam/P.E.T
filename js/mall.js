aa7c0e134c96be54c2a921d9e06d0fbfe6a01d26
HuoYanru <408074739@qq.com>
2017-12-27 10:46:25 +0800

    #

--- js/mall.js
+++ js/mall.js
@@ -10,3 +10,32 @@ var mySwiper = new Swiper ('.swiper-container', {
     autoplayDisableOnInteraction : false,
     autoplay:1500
 })
+$(function(){
+	var foot_imgs = document.getElementsByClassName('foot_nav');
+	var foot_jiahao = document.getElementsByClassName('foot_nav_jiahao');
+	function normal(){
+		foot_imgs[0].children[0].src = "../images/service_normal.png";
+		foot_imgs[1].children[0].src = "../images/circle_normal.png";
+		foot_imgs[2].children[0].src = "../images/mall_normal.png";
+		foot_imgs[3].children[0].src = "../images/my_normal.png";
+	}
+$('.foot_nav_jiahao>img').click(function(){
+	normal();
+})
+foot_imgs[0].onclick = function(){
+	normal();
+	foot_imgs[0].children[0].src = "../images/service_select.png"
+}
+foot_imgs[1].onclick = function(){
+	normal();
+	foot_imgs[1].children[0].src = "../images/circle_select.png"
+}
+foot_imgs[2].onclick = function(){
+	normal();
+	foot_imgs[2].children[0].src = "../images/mall_select.png"
+}
+foot_imgs[3].onclick = function(){
+	normal();
+	foot_imgs[3].children[0].src = "../images/my_select.png"
+	}
+})
