d3ce429f999d1d73bed46fbe457c52a0e4254967
zhangbing1612 <1013784073@qq.com>
2018-05-16 14:16:53 +0800

    代码更新

--- ionic-pet/P.E.T/src/pages/mall/mall.ts
+++ ionic-pet/P.E.T/src/pages/mall/mall.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController,ModalController, NavParams } from 'ionic-angular';
+import { Shop1Page } from '../shop1/shop1';
 
 /**
  * Generated class for the MallPage page.
@@ -14,12 +15,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'mall.html',
 })
 export class MallPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams,public modalCtrl:ModalController) {
   }
+  pet: string='food'
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MallPage');
+ i;
+  goshop1(i){
+    this.modalCtrl.create('Shop1Page',{id:i.id}).present();
+    console.log(i.id)
   }
+ shop1=JSON.parse(localStorage.getItem('res'))[0].img
+   shop=JSON.parse(this.shop1);
 
+  ionViewDidLoad() { 
+    console.log(this.shop1)
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      console.log(xhr.readyState,xhr.status);
+      if (xhr.readyState == 4) {
+        //表示服务器的相应代码是200；正确返回了数据
+        if(xhr.status == 200){
+          var message = xhr.responseText;
+          var result = JSON.parse(message);
+          localStorage.setItem('res',message)
+          
+          console.log(result)
+        }
+      }
+    };
+    xhr.open("post","http://127.0.0.1:8086?",true);//使用POST方法
+    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");//POST需增加
+    xhr.send();
+
+
+  }
 }
--- ionic-pet/P.E.T/src/pages/mine/mine.ts
+++ ionic-pet/P.E.T/src/pages/mine/mine.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CarPage } from '../car/car';
 
 /**
  * Generated class for the MinePage page.
@@ -21,5 +22,24 @@ export class MinePage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad MinePage');
   }
-  name='点点'
+  name='点点';
+  gocar(){
+    this.navCtrl.push('CarPage');
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      console.log(xhr.readyState,xhr.status);
+      if (xhr.readyState == 4) {
+        //表示服务器的相应代码是200；正确返回了数据
+        if(xhr.status == 200){
+          var message = xhr.responseText;
+          var result = JSON.parse(message);
+          console.log(result)
+        }
+      }
+    };
+    xhr.open("post","http://127.0.0.1:8086?",true);//使用POST方法
+    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");//POST需增加
+    xhr.send();
+    }
+  
 }
